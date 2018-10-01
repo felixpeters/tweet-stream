287581dee629eca77dd1c659f26f0cb2643107bc
Felix Peters <felix@albrechtpeters.de>
2018-10-01 15:52:22 +0200

    add examples

--- scripts/stream_monitoring.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-cd $1
-# send update notification
-python $PWD/scripts/update_notification.py $PWD/tweets.db "peters@is.tu-darmstadt.de" "Twitter Stream"
-# backup database file
-sh $PWD/scripts/db_backup.sh $PWD/tweets.db s3://tep-research-project/backups/
