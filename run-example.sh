94e244da006079663ee9eb40e37d5ff047e9ba5d
Felix Peters <felix@albrechtpeters.de>
2018-10-01 15:52:35 +0200

    add example scripts

---
+++ run-example.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+# run example using example IDs in data folder and saving to DB file in this
+# directory
+python -m tweet-stream
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+# create conda environment
+conda env create -f tweet-stream.yml
+# activate conda environment
+source activate tweet-stream
